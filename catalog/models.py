ae255de7e9c5ee1c4c293a0f06ffa663bc45cd26
Maria <mararyabina62@gmail.com>
2023-11-18 22:26:36 +1000

    some change

--- catalog/models.py
+++ catalog/models.py
@@ -4,11 +4,23 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Category(models.Model):
+    name_category = models.CharField(verbose_name='Наименование категории')
+    description_category = models.TextField(verbose_name='Описание категории')
+
+    def __str__(self):
+        return f'{self.name_category} {self.description_category}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
 class Product(models.Model):
     name = models.CharField(verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category = models.CharField(verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
     price = models.IntegerField(verbose_name='Цена за покупку')
     date_of_creation = models.DateTimeField(verbose_name='Дата создания')
     last_modified_date = models.DateTimeField(verbose_name='Дата последнего изменения')
@@ -20,15 +32,3 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-
-
-class Category(models.Model):
-    name_category = models.CharField(verbose_name='Наименование категории')
-    description_category = models.TextField(verbose_name='Описание категории')
-
-    def __str__(self):
-        return f'{self.name_category} {self.description_category}'
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
